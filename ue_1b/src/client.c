86813fb002506ad3220325833549e6cceb1edd51
Markus Klein <m@mklein.co.at>
2018-11-07 22:38:24 +0100

    add documentation for the client

--- ue_1b/src/client.c
+++ ue_1b/src/client.c
@@ -1,11 +1,13 @@
 /**
  * @file client.c
  * @author Markus Klein (e11707252@student.tuwien.ac.at)
- * @brief 
+ * @brief Implementation of the http client for exercise 1b.
  * @version 1.0
  * @date 2018-11-07
- * 
- * 
+ * @details Implementation of a very simplified http client which
+ * is able to perform GET requests. The code in this module consists
+ * mostly of setup code resource management while the specifics on the
+ * http protocol are provided by the http module. 
  */
 
 #include <stdio.h>
@@ -21,35 +23,153 @@
 #include "utils.h"
 #include "http.h"
 
+/**
+ * @brief Status code for protocol errors.
+ * @details The client exists with this status code if an http protocol
+ * error occurs (e.g. the server returns an invalid response).
+ */
 #define EXIT_PROTOCOL_ERR 2
+
+/**
+ * @brief Status code for http errors.
+ * @details The client exists with this status code if the server response
+ * has a status code != 200 (OK).
+ */
 #define EXIT_STATUS_ERR 3
 
+/**
+ * @brief Program name.
+ * @details Name of the executable used for usage and error messages.
+ */
 static char *progname;
 
-// These are all pointers to non allocated memory space -> no free needed.
-static char *port = "http", 
-     *outdir = NULL, 
-     *outfile = NULL;
+/**
+ * @brief Server port.
+ * Port number of the server (-p cli argument)
+ */
+static char *port = "http";
+
+/**
+ * @brief Output directory path.
+ * Output directory to which the response body should be written to.
+ * (the -d cli argument)
+ */
+static char *outdir = NULL;
+
+/**
+ * @brief Output file path.
+ * Output file where the the response body should be written to.
+ * (the -o cli argument)
+ */
+static char *outfile = NULL;
 
 // These are all pointers to allocated memory space -> free needed.
-static char *hostname = NULL,
-            *file_path = NULL;
 
-static struct addrinfo *ai = NULL;
-static FILE *sock = NULL, *out = NULL;
+/**
+ * @brief Hostname part of the URL
+ * @details Hostname part of the request URL (e.g. "localhost" in 
+ * http://localhost/somedir/index.html). 
+ * Dynamically allocated memory, needs to be freed!
+ */
+static char *hostname = NULL;
+            
+/**
+ * @brief File path part of the URL
+ * @details File path  part of the request URL (e.g. "/somedir/index.html" in 
+ * http://localhost/somedir/index.html).
+ * Dynamically allocated memory, needs to be freed!
+ */
+static char *file_path = NULL;
 
+/**
+ * @brief Server connection
+ */
+static FILE *sock = NULL;
+
+/**
+ * @brief Output stream
+ * Stream for the output file.
+ */
+static FILE *out = NULL;
+
+/**
+ * Print usage. 
+ * @brief Prints synopsis of the mydiff program.
+ * 
+ * @details Prints the usage message of the programm mydiff on sterr and 
+ * terminates the program with EXIT_FAILURE.
+ * Global variables: progname.
+ */
 static void usage(void);
 
+/**
+ * @brief Establish connection to server.
+ * @details Opens a socket connection to the server using the hostname and 
+ * port stored in the variables "hostname" and "port". The resulting stdio 
+ * stream is stored to the "sock" variable. 
+ * Global variables: hostname, port, sock.
+ */
 static void connect_to_server(void);
 
+/**
+ * @brief Handle error codes returned by functions of the http module.
+ * 
+ * @param err Error code returned by the function (a values defined in http_err_t).
+ * @param cause Description during which operation the error occured, used 
+ * for the error message printed to stdout.
+ * 
+ * @details Handles the given error by printing an appropriate error message and 
+ * initiating a sane shutdown of the program with the correponding exit status. 
+ */
 static void handle_http_err(int err, char *cause);
 
+/**
+ * Cleanup and terminate.
+ * @brief Free allocated memory, clode open streams and terminate program with the
+ * given status code.
+ * 
+ * @param status Returns status of the program.
+ * 
+ * @details Closes open file streams (pointers != NULL) and exits the program using 
+ * exit(), returning the given status.
+ * Global variables: hostname, file_path, sock, out.
+ */
 static void cleanup_exit(int status);
 
+/**
+ * @brief Sends a GET request to the server and handles the response.
+ * @details Uses the functions of the http module to send a http GET request
+ * to the server and receive the corresponding reply. As required by the exercise
+ * description the Host and Connection headers are set in the request. The request 
+ * body is written either to a file or to stdout. If the server responds with an 
+ * malformed response, the programs exists with EXIT_PROTOCOL_ERR. If a reponse
+ * status code != 200 is encountered, the exit code will be EXIT_STATUS_ERR.
+ * Global variables: hostname, file_path, sock, out.
+ */
 static void perform_request(void);
 
+/**
+ * @brief Opens the stdio stream where the response body should be written to.
+ * @details Depending on the argument passed to the program, this function either
+ * sets out to stdout (if neither the -o nor the -d option were present) or opens
+ * a stream to the output file.
+ */
 static void open_out_file(void);
 
+/**
+ * Main method for the http client.
+ * @brief Program entry point. Parses the command line arguments and calls the 
+ * functions which perform the actual http request.
+ * 
+ * @param argc Argument counter.
+ * @param argv Argument vector.
+ * @return int Program exit code (EXIT_SUCCESS)
+ * 
+ * @details Reads the command line arguments via getopt and checks for the correct 
+ * number of arguments. If the argument count and provided options are correct, the 
+ * subroutines for connecting to the server, performing the request, handling the
+ * response and shutting down the program are called. 
+ */
 int main(int argc, char **argv) {
     progname = argv[0];
     char *url = NULL;
@@ -102,7 +222,7 @@ static void usage(void) {
 }
 
 static void connect_to_server(void) {
-    struct addrinfo hints;
+    struct addrinfo hints, *ai;
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_STREAM;
@@ -115,13 +235,16 @@ static void connect_to_server(void) {
     int sockfd = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
     if(sockfd < 0) {
         ERRPRINTF("socket failed: %s\n", strerror(errno));
+        freeaddrinfo(ai);
         cleanup_exit(EXIT_FAILURE);
     }
 
     if(connect(sockfd, ai->ai_addr, ai->ai_addrlen) < 0) {
         ERRPRINTF("connect failed: %s\n", strerror(errno));
+        freeaddrinfo(ai);
         cleanup_exit(EXIT_FAILURE);
     }
+    freeaddrinfo(ai);
 
     if((sock = fdopen(sockfd, "w+")) == NULL) {
         ERRPRINTF("fdopen failed: %s\n", strerror(errno));
@@ -238,9 +361,6 @@ static void handle_http_err(int err, char *cause) {
 static void cleanup_exit(int status) {
     free(hostname);
     free(file_path);
-    if(ai != NULL) {
-        freeaddrinfo(ai);
-    }
 
     if(sock != NULL) {
         fclose(sock);
